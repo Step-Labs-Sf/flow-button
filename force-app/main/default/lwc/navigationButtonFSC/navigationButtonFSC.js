bb806e9fc4e9e0a0b0119c8f63db1df5a8be3768
Adam Stepanek <stepanek.ada.77@gmail.com>
2022-11-14 10:22:59 +0100

    new version of package, fixed value defaulting

--- force-app/main/default/lwc/navigationButtonFSC/navigationButtonFSC.js
+++ force-app/main/default/lwc/navigationButtonFSC/navigationButtonFSC.js
@@ -1,23 +1,51 @@
 import {api, LightningElement} from 'lwc';
-import {FlowNavigationBackEvent, FlowNavigationFinishEvent, FlowNavigationNextEvent, FlowNavigationPauseEvent, FlowAttributeChangeEvent} from 'lightning/flowSupport';
+import {
+    FlowNavigationBackEvent, FlowNavigationFinishEvent, FlowNavigationNextEvent, FlowNavigationPauseEvent, FlowAttributeChangeEvent
+} from 'lightning/flowSupport';
 
 export default class NavigationButtonFsc extends LightningElement {
     @api label;
-    @api variant;
-    @api type;
-    @api disabled;
+
+    _variant;
+    @api set variant(value) {
+        this._variant = value || 'neutral';
+    }
+
+    get variant() {
+        return this._variant;
+    }
+
+    _type;
+
+    @api set type(value) {
+        this._type = value || 'NEXT';
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    _disabled;
+
+    @api set disabled(value) {
+        this._disabled = value || false;
+    }
+
+    get disabled() {
+        return this._disabled;
+    }
+
     @api iconName;
     @api iconPosition;
     @api classes;
-    
+
     _innerValue;
-    
-    @api 
-    set innerValue(value) {
+
+    @api set innerValue(value) {
         this._innerValue = value;
         this.dispatchEvent(new FlowAttributeChangeEvent('innerValue', value));
     }
-    
+
     get innerValue() {
         return this._innerValue;
     }
@@ -28,7 +56,7 @@ export default class NavigationButtonFsc extends LightningElement {
         const isActionAvailable = (navigationEvent) => {
             return this.availableActions.find((action) => action === navigationEvent);
         };
-        
+
         this.innerValue = this.label;
 
         switch (this.type) {
